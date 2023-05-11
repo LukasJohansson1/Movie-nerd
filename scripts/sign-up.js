4ddb2759fef511232fa8f51d13ecc1c6803bf504
Lukas <lukas.johansson@skola.taby.se>
2023-05-11 14:16:49 +0200

    update

---
+++ scripts/sign-up.js
@@ -0,0 +1,11 @@
+function register() {
+    var nameInput = document.getElementById("name");
+    var emailInput = document.getElementById("email");
+    var passwordInput = document.getElementById("password");
+
+    nameInput.value = "";
+    emailInput.value = "";
+    passwordInput.value = "";
+
+    alert("Thank you for registering");
+  }
